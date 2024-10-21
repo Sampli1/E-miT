fec32f279db1e81ee96f8414bd170bd86392c334
Sampli1 <sandro.marghella@gmail.com>
2024-10-21 12:14:41 +0200

    Starting project

---
+++ main/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#include "esp_log.h"
+
+void app_main(void)
+{
+    char *taskName = pcTaskGetName(NULL);
+    ESP_LOGI(taskName, "Hello world!");
+
+    while (1);
+}
